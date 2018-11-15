1abec9f9a1f6ecb843e9a4a51fac03e2f4c49110
onelivesleft <iainking@gmail.com>
2018-11-15 21:47:05 +0000

    v1.3 configure()

--- PrettyErrors.py
+++ PrettyErrors.py
@@ -1,78 +1,108 @@
-# Version 1.2
+# Version 1.3
 
 import sys, re, colorama, os, time
+from enum import Enum
+
 colorama.init()
 
 
-# Mode select values for FILENAME_DISPLAY
-COMPACT  = 0
-EXTENDED = 1
-FULL     = 2
+class FilenameDisplayMode(Enum):
+    COMPACT  = 0
+    EXTENDED = 1
+    FULL     = 2
 
 
-# Control variables; change to suit your preference
-LINE_LENGTH = 50
-FILENAME_DISPLAY = COMPACT
-DISPLAY_TIMESTAMP = False
-SEPERATOR_CHARACTER = "-"
-SEPERATOR   = '\033[01;30m'
-DEFAULT     = "\033[02;37m"
-TIMESTAMP   = "\033[01;30m"
-FILENAME    = "\033[01;36m"
-LINE_NUMBER = "\033[01;32m"
-FUNCTION    = "\033[01;34m"
-
-PARTS = re.compile(r'.*File "(.*\.py)", line ([0-9]+), in (.*)')
+part_expression = re.compile(r'.*File "(.*\.py)", line ([0-9]+), in (.*)')
 
 
 class PrettyErrors():
-    def out(self, s):
-        sys.core_stderr.write(s)
+    def __init__(self):
+        self._line_length         = 79
+        self._filename_display    = FilenameDisplayMode.COMPACT
+        self._full_line_newline   = False
+        self._display_timestamp   = False
+        self._seperator_character = '-'
+        self._seperator_color     = '\033[01;30m'
+        self._timestamp_color     = '\033[01;30m'
+        self._default_color       = '\033[02;37m'
+        self._filename_color      = '\033[01;36m'
+        self._line_number_color   = '\033[01;32m'
+        self._function_color      = '\033[01;34m'
+
+
+    def configure(self, line_length = None, filename_display = None, full_line_newline = None, display_timestamp = None,
+                  seperator_character = None, seperator_color = None, default_color = None, timestamp_color = None,
+                  filename_color = None, line_number_color = None, function_color = None):
+        if line_length         is not None: self._line_length         = line_length
+        if filename_display    is not None: self._filename_display    = filename_display
+        if display_timestamp   is not None: self._display_timestamp   = display_timestamp
+        if seperator_character is not None: self._seperator_character = seperator_character
+        if seperator_color     is not None: self._seperator_color     = seperator_color
+        if default_color       is not None: self._default_color       = default_color
+        if timestamp_color     is not None: self._timestamp_color     = timestamp_color
+        if filename_color      is not None: self._filename_color      = filename_color
+        if line_number_color   is not None: self._line_number_color   = line_number_color
+        if function_color      is not None: self._function_color      = function_color
+
+
+    def out(self, s, wants_newline = False):
+        sys.pretty_errors_stderr.write(s)
+        if wants_newline and (len(s) < self._line_length or self._full_line_newline):
+            sys.pretty_errors_stderr.write('\n')
+
 
     def write(self, *args):
         for arg in args:
             for line in arg.split("\n"):
                 if line.startswith("Traceback"):
-                    if DISPLAY_TIMESTAMP:
+                    if self._display_timestamp:
                         timestamp = str(time.perf_counter())
-                        seperator = (LINE_LENGTH - len(timestamp)) * SEPERATOR_CHARACTER + timestamp
+                        seperator = (self._line_length - len(timestamp)) * self._seperator_character + timestamp
                     else:
-                        seperator = LINE_LENGTH * SEPERATOR_CHARACTER
-                    self.out("\n" + SEPERATOR + seperator + "\n")
+                        seperator = self._line_length * self._seperator_character
+                    self.out('\n')
+                    self.out(self._seperator_color + seperator, wants_newline = True)
                 else:
                     line = line.replace('\\', '/')
-                    parts = PARTS.match(line)
+                    parts = part_expression.match(line)
                     if parts:
                         line_number = parts.group(2) + " "
                         function = parts.group(3)
-                        if FILENAME_DISPLAY == FULL:
-                            filename = parts.group(1) + "\n"
-                        elif FILENAME_DISPLAY == EXTENDED:
-                            filename = parts.group(1)[-(LINE_LENGTH - len(line_number) - len(function) - 4):]
+                        wants_newline = False
+                        if self._filename_display == FilenameDisplayMode.FULL:
+                            filename = parts.group(1)
+                            wants_newline = True
+                        elif self._filename_display == FilenameDisplayMode.EXTENDED:
+                            filename = parts.group(1)[-(self._line_length - len(line_number) - len(function) - 4):]
                             if filename != parts.group(1): filename = "..." + filename
                             filename += " "
                         else:
                             filename = os.path.basename(parts.group(1)) + " "
-                        self.out("\n")
-                        self.out(FILENAME + filename)
-                        self.out(LINE_NUMBER + line_number)
-                        self.out(FUNCTION + function)
+                        self.out('\n')
+                        self.out(self._filename_color    + filename, wants_newline = wants_newline)
+                        self.out(self._line_number_color + line_number)
+                        self.out(self._function_color    + function, wants_newline = True)
                     else:
-                        self.out(DEFAULT)
-                        line.strip()
-                        while len(line) > LINE_LENGTH:
-                            c = LINE_LENGTH
+                        self.out(self._default_color)
+                        line = line.strip()
+                        while len(line) > self._line_length:
+                            c = self._line_length - 1
                             while c > 0 and line[c] not in (" ", "\t"):
                                 c -= 1
-                            if c == 0: c = LINE_LENGTH
-                            self.out(line[:c] + "\n")
+                            if c == 0: c = self._line_length
+                            self.out(line[:c], wants_newline = True)
                             line = line[c:].strip()
                         if line:
-                            self.out(line + "\n")
+                            self.out(line, wants_newline = True)
 
 
-try:
-    t = sys.core_stderr
-except AttributeError:
-    sys.core_stderr = sys.stderr
+if not getattr(sys, 'pretty_errors_stderr', False):
+    sys.pretty_errors_stderr = sys.stderr
     sys.stderr = PrettyErrors()
+
+
+def configure(line_length = None, filename_display = None, full_line_newline = None, display_timestamp = None,
+              seperator_character = None, seperator_color = None, default_color = None, timestamp_color = None,
+              filename_color = None, line_number_color = None, function_color = None):
+    sys.stderr.configure(line_length, filename_display, full_line_newline, display_timestamp, seperator_character,
+                         seperator_color, default_color, timestamp_color, filename_color, line_number_color, function_color)
--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # PrettyErrors
 
 Prettifies Python exception output to make it legible. To use simply put PrettyErrors.py in your project and
-```
+```python
 import PrettyErrors
 ```
-Note you need to be running in a terminal capable of colour output: in Windows this means powershell, cmder, etc.
+Note you need to be running in a terminal capable of colour output in order to get colour output: in Windows
+this means powershell, cmder, etc.
+
+If you want to configure the output then use `PrettyErrors.configure()`.  For example: ```python
+import PrettyErrors
+PrettyErrors.configure(seperator_character = '—', line_length = 54, filename_display = PrettyErrors.FilenameDisplayMode.FULL)
+```
 
 ![Example](https://github.com/onelivesleft/PrettyErrors/blob/master/example.png)
