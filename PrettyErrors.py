450c691bd2129be20542b045cf07664a01d4a429
onelivesleft <iainking@gmail.com>
2018-11-15 16:40:39 +0000

    v1.1 Options for filename display

--- PrettyErrors.py
+++ PrettyErrors.py
@@ -1,9 +1,18 @@
-# Version 1.0
+# Version 1.1
 
-import sys, re, colorama
+import sys, re, colorama, os
 colorama.init()
 
+
+# Mode select values for FILENAME_DISPLAY
+COMPACT  = 0
+EXTENDED = 1
+FULL     = 2
+
+
+# Control variables; change to suit your preference
 LINE_LENGTH = 50
+FILENAME_DISPLAY = COMPACT
 DEFAULT     = "\033[02;37m"
 SEPERATOR   = '\033[01;30m'
 FILENAME    = "\033[01;36m"
@@ -11,7 +20,7 @@ LINE_NUMBER = "\033[01;32m"
 FUNCTION    = "\033[01;34m"
 
 
-PARTS = re.compile(r'.*File.*/([^/]+\.py)", line ([0-9]+), in (.*)')
+PARTS = re.compile(r'.*File "(.*\.py)", line ([0-9]+), in (.*)')
 
 
 class PrettyErrors():
@@ -21,16 +30,25 @@ class PrettyErrors():
     def write(self, *args):
         for arg in args:
             for line in arg.split("\n"):
-                if line .startswith("Traceback"):
+                if line.startswith("Traceback"):
                     self.out("\n" + SEPERATOR + LINE_LENGTH * "-" + "\n")
                 else:
                     line = line.replace('\\', '/')
                     parts = PARTS.match(line)
                     if parts:
+                        line_number = parts.group(2) + " "
+                        function = parts.group(3)
+                        if FILENAME_DISPLAY == FULL:
+                            filename = parts.group(1) + "\n"
+                        elif FILENAME_DISPLAY == EXTENDED:
+                            filename = parts.group(1)[-(LINE_LENGTH - len(line_number) - len(function) - 4):]
+                            if filename != parts.group(1): filename = "..." + filename + " "
+                        else:
+                            filename = os.path.basename(parts.group(1)) + " "
                         self.out("\n")
-                        self.out(FILENAME + parts.group(1) + " ")
-                        self.out(LINE_NUMBER + parts.group(2) + " ")
-                        self.out(FUNCTION + parts.group(3))
+                        self.out(FILENAME + filename)
+                        self.out(LINE_NUMBER + line_number)
+                        self.out(FUNCTION + function)
                     else:
                         self.out(DEFAULT)
                         line.strip()
