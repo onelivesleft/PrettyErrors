8836c2db7dc40e34131cd3e8b0994b21a4b77348
onelivesleft <iainking@gmail.com>
2018-11-17 15:00:39 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pretty_errors",
-    version="1.0.7",
+    version="1.0.8",
     author="Iain King",
     author_email="iain.king@gmail.com",
     description="Prettifies Python exception output to make it legible.",
